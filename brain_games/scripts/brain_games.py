6976186f12b568cfe04c0bc8e8c8070d09dbe8e0
1437 <ichigo8106gr@gmail.com>
2024-04-05 19:55:06 +0300

    h

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Welcome to the Brain Games!")
+    user_name = input("May I have your name?")  # Запрашиваем имя у пользователя
+    print(f"Hello, {user_name}!")
+if __name__ == "__main__":
+    main()
+
