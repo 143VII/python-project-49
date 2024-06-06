caa62eedaccde8fe6e6820ce56b3e8e3b8b5f308
1437 <ichigo8106gr@gmail.com>
2024-06-06 21:29:04 +0300

    test

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.scripts.brain_game import main
-
 
 def main():
     print("Welcome to the Brain Games!")
     user_name = input("May I have your name? ")
     print(f"Hello, {user_name}!")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
